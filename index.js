baa91a0bcb282bb7720358f642650aefb93cacad
Sarbbottam Bandyopadhyay <sarbbottam@gmail.com>
2016-04-13 10:51:02 -0700

    using object lookup instead of indexOf

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 'use strict';
 module.exports = function (arr) {
 	var rest = [].concat.apply([], [].slice.call(arguments, 1));
+	var hash = {};
+
+	rest.forEach(function (item) {
+		hash[item] = true;
+	});
+
 	return arr.filter(function (el) {
-		return rest.indexOf(el) === -1;
+		return !hash[el];
 	});
 };
