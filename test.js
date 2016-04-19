fde93922789be2186a11089e134191585bb27d51
Sam Verschueren <sam.verschueren@gmail.com>
2016-04-19 19:30:59 +0200

    es2015ify

--- test.js
+++ test.js
@@ -1,6 +1,7 @@
 import test from 'ava';
 import m from './';
 
-test('filter out the difference', t => {
+test(t => {
 	t.deepEqual(m([2, 3, 4], [3, 50, 60]), [2, 4]);
+	t.deepEqual(m([2, 3, 4], [3, 50], [2, 60]), [4]);
 });
