46ef9b24f9601205a14cdeefd3d11c5e7d4ac583
Sindre Sorhus <sindresorhus@gmail.com>
2019-02-20 17:11:02 +0700

    Update test.js

--- test.js
+++ test.js
@@ -1,7 +1,7 @@
 import test from 'ava';
 import m from '.';
 
-test('returns a new array with values from second argument excluded', t => {
+test('main', t => {
 	t.deepEqual(m([2, 3, 4], [3, 50, 60]), [2, 4]);
 	t.deepEqual(m([2, 3, 4], [3, 50], [2, 60]), [4]);
 });
