9f5bef732b44e47a0b2b4e042500c4c98cabbd74
Dimitri Benin <BendingBender@users.noreply.github.com>
2019-04-08 11:01:16 +0000

    Improve TypeScript definition, refactor definition to CommonJS compatible export (#8)

--- index.test-d.ts
+++ index.test-d.ts
@@ -1,5 +1,5 @@
-import {expectType} from 'tsd-check';
-import arrayDiffer from '.';
+import {expectType} from 'tsd';
+import arrayDiffer = require('.');
 
 expectType<string[]>(arrayDiffer(['a', 'b', 'c'], ['b'], ['c']));
 expectType<number[]>(arrayDiffer([1, 2, 3], [2], [3]));
