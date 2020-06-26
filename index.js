1161cbcf496aa015461d0d854a399bd18f40183d
Sindre Sorhus <sindresorhus@gmail.com>
2020-06-26 19:10:31 +0800

    Add todo comment

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const arrayDiffer = (array, ...values) => {
+	// TODO: Make this `const rest = new Set([...values].flat());` when targeting Node.js 12.
 	const rest = new Set([].concat(...values));
 	return array.filter(element => !rest.has(element));
 };
