ad712e1b9780a4bdf8aac5e152ed6c0bce3e4491
Gunjan Kalita <gunjankalita836@gmail.com>
2020-10-06 23:21:48 +0530

    Replace array concat with ES6 and flat function

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
-'use strict';
+"use strict";
 
 const arrayDiffer = (array, ...values) => {
-	// TODO: Make this `const rest = new Set([...values].flat());` when targeting Node.js 12.
-	const rest = new Set([].concat(...values));
-	return array.filter(element => !rest.has(element));
+	const rest = new Set([...values].flat());
+	return array.filter((element) => !rest.has(element));
 };
 
 module.exports = arrayDiffer;
